b6baae34f6c2d072cb635ebf9604cd2dd32dcb7c
Nils Ponsard <nilsponsard@gmail.com>
2022-08-26 12:06:15 +0200

    start bot

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-import "github.com/NilsPonsard/verbosity"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/NilsPonsard/verbosity"
+	"github.com/nilsponsard/reaction-roles-bot-go/internal/bot"
+	"github.com/nilsponsard/reaction-roles-bot-go/internal/commands"
+)
 
 // Version will be set by the script build.sh
 var version string
 
 func main() {
+	verbosity.Debug("Starting reaction-roles-bot-go ", version)
 
-	verbosity.Debug("Starting version %s", version)
+	session, err := bot.Connect()
 
+	if err != nil {
+		verbosity.Fatal(err)
+	}
+
+	commands.Configure(session)
+
+	defer session.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
 }
